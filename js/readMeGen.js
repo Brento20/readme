a35dfbad14c7ed509d94dcb02dbd7bdf8206d10e
Brenton Weaver <brenton.weaver@gmail.com>
2022-02-06 23:24:37 +1100

    tweaked the way results are being pulled

--- js/readMeGen.js
+++ js/readMeGen.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateREADME = ({ name, email, project, what, how, who, choices, github}) =>  ` 
-# ${project} 
+const generateREADME = ({name, what, how, who, choices, email, github}) =>  ` 
+# ${answers.project} 
 
 
 
@@ -12,7 +12,7 @@ ${what}
 
 ${how}
 
-${who}
+${answers.who}
 
 
 ## Getting Started
@@ -51,44 +51,42 @@ Github: ${github}
 
 inquirer
 .prompt([
-    {
+    { // NAME
     type: 'input',
     message: 'Authors Full Name?',
     name: 'name',
     },
-    {
+    { // EMAIL
     type: 'input',
     message: 'Authors Email:',
     name: 'email',
     },
-
-    {
+    { // GITHUB USERNAME
     type: 'input',
     message: 'Github Username:',
     name: 'github',
     },
-
-    {
+    { // PROJECT TITLE
     type: 'input',
     message: 'Project Title?',
     name: 'project',
     },
-    {
+    { // WHAT
     type: 'input',
     message: 'What problem will your application solve?',
     name: 'what',
     },
-    {
+    { // HOW 
     type: 'input',
     message: 'How will your application work?',
     name: 'how',
     },
-    {
+    { // WHO
     type: 'input',
     message: 'Who will use your application solve?',
     name: 'who',
     },
-    {
+    { // TECH
     type: 'checkbox',
     message: 'What technologies used?',
     name: 'choices',
