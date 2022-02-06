d28569b49a300335ad04918df308c52db1e1cb2b
Brenton Weaver <brenton.weaver@gmail.com>
2022-02-07 00:01:41 +1100

    formating

--- js/readMeGen.js
+++ js/readMeGen.js
@@ -1,32 +1,45 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateREADME = ({name, what, how, who, choices, email, github}) =>  ` 
-# ${answers.project} 
+const generateREADME = ({project, name, what, how, who, choices, email, github, license, install, versionComments,}) =>  ` 
+# ${project} 
 
-
+## Table of Contents
+- [Description](#description)
+- [Technologies Used](#Technologies)
+- [Installing](#installing)
 
 ## Description
 
-${what}
+*${what}*
 
-${how}
+*${how}*
 
-${answers.who}
+*${who}*
 
+<br>
 
-## Getting Started
+---
 
-### Technologies Used
+## Technologies Used
 
 * ${choices}
 
-### Installing
+<br>
 
+---
+
+## Installing
+
+* ${install}
+
+<br>
+
+---
 
 ## Authors
 
-Contributors names and contact info
+Please reach out to the author below if you would like to contribute to this code.
 
 Name: ${name}
 
@@ -34,16 +47,21 @@ Email: ${email}
 
 Github: ${github}
 
+<br>
 
-
-
+---
 
 ## Version History
 
+Version Comments: ${versionComments}
 
+<br>
 
+---
 
 ## License
+
+${license}
 
 `
 
@@ -86,13 +104,33 @@ inquirer
     message: 'Who will use your application solve?',
     name: 'who',
     },
+    { // INSTALL
+    type: 'input',
+    message: 'Installation Process: ',
+    name: 'install',
+    },
     { // TECH
     type: 'checkbox',
     message: 'What technologies used?',
     name: 'choices',
     choices: ['JavaScript', 'CSS', 'HTML', 'Ruby', 'Python', 'C++'],
     },
+    { // lICENSE
+    type: "list",
+    name: "license",
+    message: "Declare licenses used: ",
+    choices: [
+        "MIT",
+        "Mozilla",
+        "Open"
+    ]
+    },
+    { // VERSION COMMENTS
+        type: 'input',
+        message: 'More details about version: ',
+        name: 'versionComments',
+        },
     
     
 ])
-.then(answers => fs.writeFile("./result/README.md", generateREADME(answers), err => err ? console.log(err) : console.log("File Made")))
+.then(answers => fs.writeFile("./result/README.md", generateREADME(answers), err => err ? console.log(err) : console.log("✔️ File Made")))
---
+++ js/result/README.md
@@ -0,0 +1,59 @@
+ 
+# Read Me Version 1.0 
+
+## Table of Contents
+- [Description](#description)
+- [Technologies Used](#Technologies)
+- [Installing](#installing)
+
+## Description
+
+*This application makes it simple for developers to share consistent standards of information shared between applications.*
+
+*By asking for specific information and displaying it in a pre formatted readme file.*
+
+*This will be utilized mostly for developers and project managers.*
+
+ <br>
+
+---
+
+## Technologies Used
+
+* CSS
+
+
+---
+
+## Installing
+
+* download the supporting files from this REPO, run npm install then run "node readMeGen.js" in your terminal
+
+
+---
+
+## Authors
+
+Contributors names and contact info
+
+Name: Brenton Weaver
+
+Email: brenton.weaver@gmail.com
+
+Github: brento20
+
+
+---
+
+## Version History
+
+Version Comments: This is the inital version and may contain bugs. Please contact the repo owner with suggestions.
+
+
+
+---
+
+## License
+
+MIT
+
